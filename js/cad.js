ae090435aa29b73e633520a752cf8097060c1f1d
sophiaborsatto <sophia-borsatto@hotmail.com>
2025-05-15 20:27:38 -0300

    cadastroUsuarios1505

--- js/cad.js
+++ js/cad.js
@@ -1,18 +1,60 @@
-//cadastro
 //json: objetos do javascript representados por {chaves} 
 //{'nome': 'maria'}
-const botao = document.getElementById('btnCadastrar');
+// || = OU
 
-botao.addEventListener('click', 
-    function(){
-        const user = {
+// cadastro 
+const botao = document.getElementById('btnCadastrar');
+//let usuarios = [];
+
+// cadastrar
+botao.addEventListener('click',
+    function () {
+        let usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
+        const usuario = {
             login: document.getElementById('login').value,
             senha: document.getElementById('senha').value
-        }  
-        let users = [];
-        users.push(user); //lista - entrada
-        let listaUsuarios = JSON.stringify(users); //criar uma memoria e guarda nele JSON
-        localStorage.setItem("users", listaUsuarios); //set na memoria, listas de usuarios (ARMAZENANDO NA MEMORIA)
-
+        }
+        usuarios.push(usuario);
+        let listaUsuarios = JSON.stringify(usuarios);
+        localStorage.setItem("usuarios", listaUsuarios);
+        document.getElementById('login').value = '';
+        document.getElementById('senha').value = ''; 
+        listar();
     }
 );
+
+//listar
+function listar(){
+    const listaUsuarioCad = JSON.parse(localStorage.getItem("usuarios")) || [];
+    console.log(listaUsuarioCad);
+    const tabelaListaUsuarios = document.getElementById('listaUsuarios');
+    tabelaListaUsuarios.innerHTML = "";
+
+    listaUsuarioCad.forEach((usuario, index) => {
+        const linha = document.createElement('tr');
+        linha.innerHTML = `
+            <td>${usuario.login}</td>
+            <td>${usuario.senha}</td>
+            <td>
+                <button onclick="editarUsuario(${index})">Editar</button>
+                <button onclick="excluirUsuario(${index})">Remover</button>
+            </td>
+        `;
+        tabelaListaUsuarios.appendChild(linha);
+    });
+}
+
+//remover
+function excluirUsuario(index){
+    const listaUsuariosCadastrados = JSON.parse(localStorage.getItem("usuarios"))||[];
+
+    if (confirm("Você realmente deseja excluir?")){
+        listaUsuariosCadastrados.splice(index, 1);
+        listaJson = JSON.stringify(listaUsuariosCadastrados);
+        localStorage.setItem("usuarios", listaJson);
+        listar();
+    }
+    
+}
+
+listar();
